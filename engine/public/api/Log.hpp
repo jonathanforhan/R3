847f0c29e2888691274cfbed8a8b22c50ed302f6
jonathanforhan <jonathan.forhan@gmail.com>
2023-12-10 16:22:05 -0500

    add Clang and GCC compile support in cmake presets

--- engine/public/api/Log.hpp
+++ engine/public/api/Log.hpp
@@ -22,24 +22,24 @@ inline void _LOG(T x, Args&&... args) {
 } // namespace R3::detail::logging
 
 #if R3_LOG_VERBOSE
-#define R3_LOG_Verbose ::R3::detail::logging::_LOG
+#define R3_LOG_Verbose(...) ::R3::detail::logging::_LOG(__VA_ARGS__)
 #else
-#define R3_LOG_Verbose void(0)
+#define R3_LOG_Verbose(...) void(0)
 #endif
 #if R3_LOG_INFO
-#define R3_LOG_Info ::R3::detail::logging::_LOG
+#define R3_LOG_Info(...) ::R3::detail::logging::_LOG(__VA_ARGS__)
 #else
-#define R3_LOG_Info void(0)
+#define R3_LOG_Info(...) void(0)
 #endif
 #if R3_LOG_WARNING
-#define R3_LOG_Warning ::R3::detail::logging::_LOG
+#define R3_LOG_Warning(...) ::R3::detail::logging::_LOG(__VA_ARGS__)
 #else
-#define R3_LOG_Warning void(0)
+#define R3_LOG_Warning(...) void(0)
 #endif
 #if R3_LOG_ERROR
-#define R3_LOG_Error ::R3::detail::logging::_LOG
+#define R3_LOG_Error(...) ::R3::detail::logging::_LOG(__VA_ARGS__)
 #else
-#define R3_LOG_Error void(0)
+#define R3_LOG_Error(...) void(0)
 #endif
 
 #define LOG(_Level, ...)                                 \
--- engine/public/render/NativeRenderObject.hpp
+++ engine/public/render/NativeRenderObject.hpp
@@ -59,29 +59,25 @@ public:
     /// @brief Return handle as a new type, used for C -> C++ bindings
     /// @tparam T cast to T type
     /// @return Type T Object from Handle
-#if R3_VULKAN
     template <typename T>
     constexpr T as() {
         CHECK(validHandle());
-        return T(reinterpret_cast<T::NativeType>(m_handle.get()));
+        if constexpr (IsWrapper<T>) {
+            return T(reinterpret_cast<T::NativeType>(m_handle.get()));
+        } else {
+            return T(m_handle.get());
+        }
     }
+
     template <typename T>
     constexpr T as() const {
         CHECK(validHandle());
-        return T(reinterpret_cast<const T::NativeType>(m_handle.get()));
+        if constexpr (IsWrapper<T>) {
+            return T(reinterpret_cast<T::NativeType>(m_handle.get()));
+        } else {
+            return T(m_handle.get());
+        }
     }
-#else
-    template <typename T>
-    constexpr T as() {
-        CHECK(validHandle());
-        return T(*m_handle);
-    }
-    template <typename T> const
-    constexpr T as() const {
-        CHECK(validHandle());
-        return T(*m_handle);
-    }
-#endif
 
 protected:
     /// @brief Set handle
